ddd0205319ffc0ad42ad6fb6cc0bf7c97d79b30f
goserg <gor-serg@bk.ru>
2021-03-03 23:15:46 +0300

    added NotEq, Like and NotLike tests

--- parser_test.go
+++ parser_test.go
@@ -11,24 +11,54 @@ type addTest struct {
 	expected interface{}
 }
 
-var addTests = []addTest{
-	{"Foo.Bar.X = 'hello'", squirrel.Eq{"Foo.Bar.X": "hello"}},
-	{"Bar.Alpha = 7", squirrel.Eq{"Bar.Alpha": "7"}},
+func TestParsers(t *testing.T) {
+	var addTests = []addTest{
+		{"Foo.Bar.X = 'hello'", squirrel.Eq{"Foo.Bar.X": "hello"}},
+		{"Bar.Alpha = 7", squirrel.Eq{"Bar.Alpha": "7"}},
+	}
+	for _, test := range addTests {
+		runTest(t, test)
+	}
 }
 
-func TestParsers(t *testing.T) {
-	for _, test := range addTests {
-		subQ := squirrel.Select("aa").From("bb")
-		got, err := Parse(test.arg, subQ)
-		gotSQL, gotArgs, _ := got.ToSql()
-		if err != nil {
-			t.Errorf(`Parse(%s, db) error = %d; want error = nil`, test.arg, err)
-		}
+func runTest(t *testing.T, test addTest) {
+	subQ := squirrel.Select("aa").From("bb")
+	got, err := Parse(test.arg, subQ)
+	gotSQL, gotArgs, _ := got.ToSql()
+	if err != nil {
+		t.Errorf(`Parse(%s, db) error = %v; want error = nil`, test.arg, err)
+	}
 
-		want := subQ.Where(test.expected)
-		wantSQL, wantArgs, _ := want.ToSql()
-		if wantSQL != gotSQL || wantArgs[0] != gotArgs[0] {
-			t.Errorf(`Parse(%s, db): got sql = "%s", want "%s"; got arg = %d, want %d`, test.arg, gotSQL, wantSQL, gotArgs[0], wantArgs[0])
-		}
+	want := subQ.Where(test.expected)
+	wantSQL, wantArgs, _ := want.ToSql()
+	if wantSQL != gotSQL || wantArgs[0] != gotArgs[0] {
+		t.Errorf(`Parse(%s, db): got sql = "%v", want "%v"; got arg = %v, want %v`, test.arg, gotSQL, wantSQL, gotArgs[0], wantArgs[0])
+	}
+}
+
+func TestParserNotEqual(t *testing.T) {
+	var addTests = []addTest{
+		{"Foo.Bar.X != 'hello'", squirrel.NotEq{"Foo.Bar.X": "hello"}},
+	}
+	for _, test := range addTests {
+		runTest(t, test)
+	}
+}
+
+func TestParserLike(t *testing.T) {
+	var addTests = []addTest{
+		{"Alice.Name ~ 'A.'", squirrel.Like{"Alice.Name": "A."}},
+	}
+	for _, test := range addTests {
+		runTest(t, test)
+	}
+}
+
+func TestParserNotLike(t *testing.T) {
+	var addTests = []addTest{
+		{"Bob.LastName !~ 'Bill.'", squirrel.NotLike{"Bob.LastName": "Bill."}},
+	}
+	for _, test := range addTests {
+		runTest(t, test)
 	}
 }
