f8cb5a22b4aec4b0ff0b2d64f51c49ddddf4a16d
goserg <gor-serg@bk.ru>
2021-03-03 23:16:29 +0300

    working on more parsing options (!=, ~, !~)

--- parser.go
+++ parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"errors"
 	"strings"
 
 	"github.com/Masterminds/squirrel"
@@ -11,6 +12,24 @@ func Parse(query string, db squirrel.SelectBuilder) (*squirrel.SelectBuilder, er
 	left := strings.Split(query, " ")[0]
 	right := strings.Split(query, " ")[2]
 	right = strings.Trim(right, `'"`)
-	db = db.Where(squirrel.Eq{left: right})
+
+	middle := strings.Split(query, " ")[1]
+
+	switch middle {
+	case "=":
+		db = db.Where(squirrel.Eq{left: right})
+		break
+	case "~":
+		db = db.Where(squirrel.Like{left: right})
+		break
+	case "!=":
+		db = db.Where(squirrel.NotEq{left: right})
+		break
+	case "!~":
+		db = db.Where(squirrel.NotLike{left: right})
+		break
+	default:
+		return nil, errors.New("can't parse it")
+	}
 	return &db, nil
 }
