b74ee3faed63d2aa49865fc7fff0dfed051686a0
goserg <gor-serg@bk.ru>
2021-03-04 18:48:02 +0300

    Refactoring, method extraction.

--- parser.go
+++ parser.go
@@ -7,29 +7,57 @@ import (
 	"github.com/Masterminds/squirrel"
 )
 
-//Parse - простой парсер WHERE части запросов SQL
-func Parse(query string, db squirrel.SelectBuilder) (*squirrel.SelectBuilder, error) {
-	left := strings.Split(query, " ")[0]
-	right := strings.Split(query, " ")[2]
-	right = strings.Trim(right, `'"`)
-
-	middle := strings.Split(query, " ")[1]
-
+func parseExpression(left string, middle string, right string) interface{} {
+	left = strings.Trim(left, `"`)
+	right = strings.Trim(right, `'`)
+	var builder interface{}
 	switch middle {
 	case "=":
-		db = db.Where(squirrel.Eq{left: right})
-		break
-	case "~":
-		db = db.Where(squirrel.Like{left: right})
+		builder = squirrel.Eq{left: right}
 		break
 	case "!=":
-		db = db.Where(squirrel.NotEq{left: right})
+		builder = squirrel.NotEq{left: right}
+		break
+	case "~":
+		builder = squirrel.Like{left: right}
 		break
 	case "!~":
-		db = db.Where(squirrel.NotLike{left: right})
+		builder = squirrel.NotLike{left: right}
 		break
-	default:
-		return nil, errors.New("can't parse it")
+	case "~*":
+		builder = squirrel.ILike{left: right}
+		break
+	case "!~*":
+		builder = squirrel.NotILike{left: right}
+		break
+	case "<>":
+		builder = squirrel.NotEq{left: right}
+		break
+	case ">":
+		builder = squirrel.Gt{left: right}
+		break
+	case ">=":
+		builder = squirrel.GtOrEq{left: right}
+		break
+	case "<":
+		builder = squirrel.Lt{left: right}
+		break
+	case "<=":
+		builder = squirrel.LtOrEq{left: right}
+		break
+	}
+	return builder
+}
+
+//Parse - простой парсер WHERE части запросов SQL
+func Parse(query string, db squirrel.SelectBuilder) (*squirrel.SelectBuilder, error) {
+	units := strings.Split(query, " ")
+	if len(units) < 3 {
+		return nil, errors.New("Parsing error")
+	}
+	if len(units) == 3 {
+		expression := parseExpression(units[0], units[1], units[2])
+		db = db.Where(expression)
 	}
 	return &db, nil
 }
