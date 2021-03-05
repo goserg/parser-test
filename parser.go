b19dacd738a884af4039f78b55d694b0a3756f29
goserg <gor-serg@bk.ru>
2021-03-05 19:21:36 +0300

    reworked parser algorithm

--- parser.go
+++ parser.go
@@ -7,10 +7,9 @@ import (
 	"github.com/Masterminds/squirrel"
 )
 
-func parseExpression(left string, middle string, right string) interface{} {
-	left = strings.Trim(left, `"`)
+func parseExpression(left string, middle string, right string) squirrel.Sqlizer {
 	right = strings.Trim(right, `'`)
-	var builder interface{}
+	var builder squirrel.Sqlizer
 	switch middle {
 	case "=":
 		builder = squirrel.Eq{left: right}
@@ -49,15 +48,91 @@ func parseExpression(left string, middle string, right string) interface{} {
 	return builder
 }
 
+func getNextUnit(query string) (string, string) {
+	query = strings.Trim(query, " ")
+	indexOfFirstSpace := strings.Index(query, " ")
+	if indexOfFirstSpace == -1 {
+		return query, ""
+	}
+	return query[:indexOfFirstSpace], query[indexOfFirstSpace+1:]
+}
+
+func getNextExpr(query string) (squirrel.Sqlizer, string, error) {
+	var sign, left, right string
+	left, query = getNextUnit(query)
+	sign, _ = getNextUnit(query)
+	if isAComparisonSign(sign) {
+		_, query = getNextUnit(query)
+		right, query = getNextUnit(query)
+		return parseExpression(left, sign, right), query, nil
+	}
+	if isABooleanSign(sign) {
+		return squirrel.Eq{left: true}, query, nil
+	}
+	return nil, query, errors.New("Parsing error")
+}
+
+func getNextANDBlock(query string) (squirrel.Sqlizer, string, error) {
+	var expr, nextExpr squirrel.Sqlizer
+	var err error
+	expr, query, err = getNextExpr(query)
+	if err != nil {
+		return nil, "", errors.New("Parsing error")
+	}
+	sign, _ := getNextUnit(query)
+	for sign == "AND" || sign == "and" {
+		_, query = getNextUnit(query)
+		nextExpr, query, err = getNextExpr(query)
+		if err != nil {
+			return nil, "", errors.New("Parsing error")
+		}
+		expr = squirrel.And{expr, nextExpr}
+		sign, _ = getNextUnit(query)
+	}
+	return expr, query, nil
+}
+
+func isAComparisonSign(elem string) bool {
+	for _, comp := range []string{"=", "!=", "<>", "~", "!~", "~*", "!~*", ">", ">=", "<", "<="} {
+		if comp == elem {
+			return true
+		}
+	}
+	return false
+}
+
+func isABooleanSign(elem string) bool {
+	for _, comp := range []string{"AND", "OR", "and", "or"} {
+		if comp == elem {
+			return true
+		}
+	}
+	return false
+}
+
 //Parse - простой парсер WHERE части запросов SQL
 func Parse(query string, db squirrel.SelectBuilder) (*squirrel.SelectBuilder, error) {
-	units := strings.Split(query, " ")
-	if len(units) < 3 {
-		return nil, errors.New("Parsing error")
+	var expression, rightExpr, empty squirrel.Sqlizer
+	var sign string
+	var err error
+	for query != "" {
+		if expression == empty {
+			expression, query, err = getNextANDBlock(query)
+			if err != nil {
+				return nil, errors.New("Parsing error")
+			}
+		}
+		sign, query = getNextUnit(query)
+		if isABooleanSign(sign) {
+			if sign == "OR" || sign == "or" {
+				rightExpr, query, err = getNextANDBlock(query)
+				if err != nil {
+					return nil, errors.New("Parsing error")
+				}
+				expression = squirrel.Or{expression, rightExpr}
+			}
+		}
 	}
-	if len(units) == 3 {
-		expression := parseExpression(units[0], units[1], units[2])
-		db = db.Where(expression)
-	}
+	db = db.Where(expression)
 	return &db, nil
 }
