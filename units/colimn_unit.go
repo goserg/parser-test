da382fe1fa7dac9d0a8f4366390c933d2b5f6c3e
goserg <gor-serg@bk.ru>
2021-03-06 23:41:03 +0300

    Finished name validity check

--- units/colimn_unit.go
+++ units/colimn_unit.go
@@ -1,6 +1,9 @@
 package units
 
-import "strings"
+import (
+	"strings"
+	"unicode"
+)
 
 //ColumnUnit структура для хранения имени колонки
 type ColumnUnit struct {
@@ -9,8 +12,21 @@ type ColumnUnit struct {
 
 //IsValid проверка валидности имени колонки
 func (c *ColumnUnit) IsValid() bool {
-	if strings.Index("0123456789", string(c.Name[0])) != -1 {
-		return false
+	firstSymbol := string(c.Name[0])
+	lastSymbol := string(c.Name[len(c.Name)-1])
+	if firstSymbol == `"` && lastSymbol == `"` { // К идентификаторам в кавычках не применяются правила валидности
+		return true
+	}
+	for i, symbol := range c.Name {
+		if i == 0 {
+			if !unicode.IsLetter(symbol) && symbol != rune("_"[0]) {
+				return false
+			}
+		} else {
+			if !unicode.IsLetter(symbol) && strings.Index("0123456789_.", string(symbol)) == -1 {
+				return false
+			}
+		}
 	}
 
 	return true
